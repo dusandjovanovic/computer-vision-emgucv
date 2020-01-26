e6879cb65d3200b8d6a4fc2feae479801d06c58a
Dusan Jovanovic <dusandjovanovic@outlook.com>
2020-01-26 20:12:23 +0100

    Chane build settings: .NET framework instead of .NET Core (Emgu is not compatible)

---
+++ computer-vision-app/ComputerVisionApp/ComputerVisionApp/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using Emgu.CV;
+
+namespace ComputerVisionApp
+{
+    public partial class MainWindow : Window
+    {
+        private string ImagePath;
+        private BitmapImage ImageBitmap;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dialog = new Microsoft.Win32.OpenFileDialog();
+            dialog.Filter = "Image Files (*.bmp;*.jpg;*.jpeg,*.png)|*.BMP;*.JPG;*.JPEG;*.PNG";
+            if (dialog.ShowDialog() == true)
+            {
+                this.ImagePath = dialog.FileName;
+                this.ImageBitmap = new BitmapImage(new Uri(this.ImagePath));
+                ImageContainer.Source = this.ImageBitmap;
+            }
+        }
+    }
+}
